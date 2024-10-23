8e50f74befcd9830c44d8f8a001b1080b4870787
Juicy <iamjuicylife@gmail.com>
2024-10-23 12:29:13 -0600

    the beginning

---
+++ www/cordova-plugin-imagegallery.js
@@ -0,0 +1,23 @@
+var exec = require('cordova/exec');
+exports.ensurePermissions = function(callback,failureCallback) {//ios
+  return exec(callback, failureCallback, "CDVImageGallery", "ensurePermissions", []);
+};
+exports.hasReadPermission = function(callback,failureCallback) {//android
+  return exec(callback, failureCallback, "CDVImageGallery", "hasReadPermission", []);
+};
+exports.show=function(success,error,options){
+	if (!options) {
+		options = {};
+	}
+	var params = {
+		mode:(options.mode)?options.mode:'LibraryOnly',
+        gridSize: options.gridSize ? options.gridSize : 3,
+        cellSpacing: options.cellSpacing ? options.cellSpacing : 2,
+        maxDuration: options.maxDuration ? options.maxDuration : 2,
+        maximumImagesCount: options.maximumImagesCount ? options.maximumImagesCount : 15,
+		width: 0,//legacy, not supported in ios
+		height:0,//legacy, not supported in ios
+		quality: options.quality ? options.quality : 100
+	};
+	exec(success, error, "CDVImageGallery", "show", [params]);
+}
